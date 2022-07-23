e12f2f8281736741959853e3c08683d64e955a87
Keep Zen <keep.in.zen@pm.me>
2022-07-24 03:11:29 +0800

    v4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TheCornerOfElixir.MixProject do
   def project do
     [
       app: :the_corner_of_elixir,
-      version: "0.1.0",
+      version: "4.0.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
