d77da850046c15ab44a38788c223bfd1ab45bb4b
KeepZen <keep.in.zen@pm.me>
2022-07-24 13:33:10 +0000

    Deploying to book from @ KeepZen/the-corner-of-elixir@4a7407fad2aad953a7e550f836652aea5db71da1 🚀

---
+++ en/dist/sidebar_items-db8fc97afc.js
@@ -0,0 +1,1 @@
+sidebarNodes={"extras":[{"group":"","headers":[{"anchor":"the-corner-of-the-elixir","id":"The Corner of the Elixir"}],"id":"readme","title":"ReadMe"},{"group":"","headers":[{"anchor":"the-corner-of-elixir","id":"The Corner of Elixir"}],"id":"index_en","title":"Content"}],"modules":[],"tasks":[]}
